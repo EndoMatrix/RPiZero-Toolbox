689ba8f6628d9e6a7ca5fa930d88ef2ecb585dad
Matt Barber <mattbarbers@btinternet.com>
2019-06-24 19:00:30 +0100

    refactor switch statement to if-else statement

--- nodejs/app.js
+++ nodejs/app.js
@@ -24,14 +24,10 @@ const run = async (debug) => {
     const UUID_36 = parseInt(address.mac.replace(/:/g, '').substr(-6), 16).toString(36).toUpperCase(); // hexatridecimal encoding for latter half of MAC address
     const UUID_16 = ble.toUUID('A35F64F1-0000-0000-0000-6D2F4064C7FA'); // hexadecimal encoding for Service UUID
 
-    switch (state) {
-      case 'poweredOn':
-        bleno.startAdvertising('Raspberry Pi ' + UUID_36, [UUID_16]);
-        break;
-      default:
-        bleno.stopAdvertising();
-        break;
-    }
+    if (state.toUpperCase() === 'POWEREDON')
+      bleno.startAdvertising('Raspberry Pi ' + UUID_36, [UUID_16]);
+    else
+      bleno.stopAdvertising();
   });
 };
 
