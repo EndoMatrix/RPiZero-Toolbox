29c303780716dd97772e10b9d26110aeb3a03ab0
Matt Barber <mattbarbers@btinternet.com>
2019-06-24 17:43:56 +0100

    encapsulate app.js startup with async call

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,24 +1,38 @@
-const bleno = require('bleno');
+// local dependencies
+const debug = require('./tools/dbg-helper.js');
 
-const run = require('./services/run/service.js');
-const ble = require('./tools/ble-helper.js');
+const run = async (debug) => {
+  debug.app('fetching external modules...');
+  const bleno = require('bleno');
+  debug.app('success!');
 
-bleno.setServices([run.service], (error) => {
-  if (error) throw error
-});
+  debug.app('fetching internal modules...');
+  const ble = require('./tools/ble-helper.js');
+  const run = require('./services/run/service.js');
+  debug.app('success!');
 
-const [address] = require('os').networkInterfaces().wlan0;
+  debug.bleno('bleno');
+  debug.noble('noble');
 
-bleno.on('stateChange', (state) => {
-  const UUID_36 = parseInt(address.mac.replace(/:/g, '').substr(-6), 16).toString(36).toUpperCase(); // hexatridecimal encoding for latter half of MAC address
-  const UUID_16 = ble.toUUID('A35F64F1-0000-0000-0000-6D2F4064C7FA'); // hexadecimal encoding for Service UUID
+  debug.app('init');
+  bleno.setServices([run.service], (error) => {
+    if (error) debug.bleno(error);
+  });
 
-  switch (state) {
-    case 'poweredOn':
-      bleno.startAdvertising('Raspberry Pi ' + UUID_36, [UUID_16]);
-      break;
-    default:
-      bleno.stopAdvertising();
-      break;
-  }
-});
+  const [address] = require('os').networkInterfaces().wlan0;
+
+  bleno.on('stateChange', (state) => {
+    const UUID_36 = parseInt(address.mac.replace(/:/g, '').substr(-6), 16).toString(36).toUpperCase(); // hexatridecimal encoding for latter half of MAC address
+    const UUID_16 = ble.toUUID('A35F64F1-0000-0000-0000-6D2F4064C7FA'); // hexadecimal encoding for Service UUID
+
+    switch (state) {
+      case 'poweredOn':
+        bleno.startAdvertising('Raspberry Pi ' + UUID_36, [UUID_16]);
+        break;
+      default:
+        bleno.stopAdvertising();
+        break;
+    }
+  });
+};
+run(debug);
